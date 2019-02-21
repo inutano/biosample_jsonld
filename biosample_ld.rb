31b6016c83493f7f59e54ee097c34102522f9928
Tazro Inutano Ohta <inutano@gmail.com>
2019-02-21 18:13:29 +0900

    save data in files

--- biosample_ld.rb
+++ biosample_ld.rb
@@ -6,17 +6,13 @@ if __FILE__ == $0
   bsid = ARGV.first
   bs = BioSample.new(bsid)
 
-  jsonld = JSON.dump(bs.data)
-  puts jsonld
-
-  ttl = bs.to_ttl
-  puts ttl
-
   # Write json-ld
-  # path_to_jsonld = File.join("./data/jsonld", bsid+".jsonld")
-  # open(path_to_jsonld,"w"){|f| f.puts(jsonld) }
+  jsonld = JSON.dump(bs.data)
+  path_to_jsonld = "./#{bsid}.jsonld"
+  open(path_to_jsonld,"w"){|f| f.puts(jsonld) }
 
   # Write ttl
-  # path_to_ttl = File.join("./data/ttl", bsid+".ttl")
-  # open(path_to_ttl,"w"){|f| f.puts(ttl) }
+  ttl = bs.to_ttl
+  path_to_ttl = "./#{bsid}.ttl"
+  open(path_to_ttl,"w"){|f| f.puts(ttl) }
 end
