66226bfb04321a57ce2f99944e2d06da6402e2fb
Tazro Inutano Ohta <inutano@gmail.com>
2020-06-16 16:09:01 +0900

    update job file using docker container

--- sh/biosample.job.sh
+++ sh/biosample.job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #$ -S /bin/bash -j y
 # set -eux
+module load docker
 
 XML_PATH="${1}"
 jobconf="${2}"
@@ -15,19 +16,30 @@ job_name="biosample.$(echo ${job_param} | sed -e 's:,.*$::')"
 ttl_path="${TTL_DIR}/${job_name}.ttl"
 
 if [[ ! -e "${ttl_path}" ]]; then
-  # Generate a subset of xml and generate ttl
-  cd "/home/inutano/repos/biosample_jsonld"
-  ./bs2ld xml2ttl <(cat <(echo "<BioSampleSet>") <(cat "${XML_PATH}" | sed -n "${job_param}")) > "${ttl_path}"
+  tmp_xml=${ttl_path}.xml
+  printf "<BioSampleSet>\n$(cat "${XML_PATH}" | sed -n "${job_param}")" > ${tmp_xml}
+
+  docker run --security-opt seccomp=unconfined --rm -t \
+    -e TZ=Asia/Tokyo \
+    --volume ${TTL_DIR}:/work \
+    "quay.io/inutano/biosample_jsonld:v1.3" \
+    bs2ld \
+    xml2ttl \
+    /work/$(basename ${tmp_xml}) \
+    > "${ttl_path}"
+  rm -f ${tmp_xml}
 fi
 
 # validate
 validation_output="${ttl_path}.validation"
 valid_value='Validator finished with 0 warnings and 0 errors.'
 
-module load docker
-image="quay.io/inutano/turtle-validator:v1.0"
-
-docker run --rm -v $(dirname "${ttl_path}"):/work "${image}" ttl $(basename "${ttl_path}") > "${validation_output}"
+docker run --security-opt seccomp=unconfined --rm -t \
+   -v $(dirname "${ttl_path}"):/work \
+   "quay.io/inutano/turtle-validator:v1.0" \
+   ttl \
+   /work/$(basename "${ttl_path}") \
+   > "${validation_output}"``
 
 if [[ $(cat "${validation_output}") == "${valid_value}" ]]; then
   rm -f "${validation_output}"
